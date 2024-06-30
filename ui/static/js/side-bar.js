38b27fb4876c10f40d89153259386c601890a3da
jmarren <jmarren99@gmail.com>
2024-06-29 20:43:16 -0700

    custom cursor-1

--- ui/static/js/side-bar.js
+++ ui/static/js/side-bar.js
@@ -12,21 +12,45 @@ class SideBar extends HTMLElement {
 
     template.innerHTML = `
       <style>
-        #side-bar-container {
-          position: fixed;
-          top: 50px;
-          left: 0;
-          z-index: -1;
-          height: 100%;
-          width: 125px;
-          border-right: 4px solid black;
-          background-color: seagreen;
+      #side-bar-container {
+        position: fixed;
+        top: 50px;
+        left: 0;
+        z-index: -1;
+        height: 100%;
+        border-right: 4px solid black;
+        background-color: seagreen;
+        padding-top: 100px;
       }
+      nav {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+      }
+      .side-bar-item {
+        text-align: center;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        border-bottom: 2px solid darkseagreen;
+        font-family: Cheto;
+        font-size: 30px;
+        color: cornsilk;
+        }
+      .side-bar-item:nth-of-type(1) {
+        border-top: 2px solid darkseagreen;
+      }
+      
 
       </style> 
 
         <div id="side-bar-container"> 
-          
+          <nav>
+            <div id="profile" class="side-bar-item">Profile</div>
+            <div id="games" class="side-bar-item">Games</div>
+            <div id="friends" class="side-bar-item">Friends</div>
+          </nav>
         </div>
 `
 
