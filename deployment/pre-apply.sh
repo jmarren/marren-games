22237864a39c548e495436029086e0bebfe5bf7f
jmarren <jmarren99@gmail.com>
2024-08-08 10:42:59 -0700

    removed js from pre-apply.sh; removed currently unused preload extension

--- deployment/pre-apply.sh
+++ deployment/pre-apply.sh
@@ -7,7 +7,6 @@ echo "****** syncing ******** "
 # copy all necessary files to s3 bucket
 aws s3 sync ../ui/templates s3://ask-away-s3-bucket/app/ui/templates
 aws s3 sync ../ui/static/public s3://ask-away-s3-bucket/public/static
-aws s3 sync ../ui/static/js s3://ask-away-s3-bucket/public/js
 aws s3 sync ../sql s3://ask-away-s3-bucket/app/sql
 aws s3 cp ./build s3://ask-away-s3-bucket/app/build
 aws s3 cp ./nginx.conf s3://ask-away-s3-bucket/app/nginx.conf
